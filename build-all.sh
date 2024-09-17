3d221cc301c14ef8c842cdd86be58e4f412220a5
Bryan Gemler <gemler.1@osu.edu>
2024-09-16 21:05:21 -0400

    fixing comma bug

--- build-all.sh
+++ build-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-DockerContainers=("Nano-HostRemoval" "Nano-K2TaxClass" "Nano-QualReport" "Nano-ReadTrim" "Nano-SummarizeUtils" "Nano-Assembly")
+DockerContainers=("Nano-HostRemoval" "Nano-K2TaxClass" "Nano-QualReport" "Nano-ReadTrim" "Nano-SummarizeUtils" "Nano-Assembly" "Nano-SNP")
 
 for container in ${DockerContainers[@]}; do
     # Enter container
